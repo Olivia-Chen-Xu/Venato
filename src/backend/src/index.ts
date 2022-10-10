05cbb6823cb5c372f05716722ab4ea75acd26006
Reid Moffat <18rem8@queensu.ca>
2022-10-09 23:35:43 -0400

    added more auth functions

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,40 +1,48 @@
 import * as functions from 'firebase-functions';
 import admin = require('firebase-admin');
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../../config/firebase';
 
 /**
  * Configure firebase functions
- * Firebase admin is required for functions to access firestore
+ * Firebase admin is required for functions to access firestore (for security)
  */
 admin.initializeApp();
+const adminAuth = admin.auth();
 // const db = admin.firestore();
-const auth = admin.auth();
 
 /**
- * Helper functions (not called directly)
+ * Helper functions & interfaces
  */
+
+interface LoginReq {
+    email: string;
+    password: string;
+}
+
+interface CreateUserReq extends LoginReq {
+    displayName: string;
+}
 
 /**
  * Firebase functions to be deployed
  */
 
-interface CreateUserReq {
-    email: string;
-    password: string;
-    displayName: string;
-}
+export const signup = functions.https.onCall((data: CreateUserReq, context) => {
+    const req = {
+        email: data.email,
+        emailVerified: false,
+        password: data.password,
+        displayName: data.displayName,
+        disabled: false,
+    };
+    return adminAuth.createUser(req);
+});
 
-export const createUser = functions.https.onCall(
-    (data: CreateUserReq, context) => {
-        return auth.createUser({
-            email: data.email,
-            emailVerified: false,
-            password: data.password,
-            displayName: data.displayName,
-            disabled: false,
-        });
-    }
-);
+export const signin = functions.https.onCall((data: LoginReq, context) => {
+    return signInWithEmailAndPassword(auth, data.email, data.password);
+});
 
-export const testFunc = functions.https.onCall(
-    (data, context) => 'test function'
-);
+export const signout = functions.https.onCall((data, context) => {
+    return signOut(auth);
+});
