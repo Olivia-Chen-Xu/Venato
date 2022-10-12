b3de95c859662a6bcff4f234a5a0112efca6a91b
Reid Moffat <18rem8@queensu.ca>
2022-10-12 11:58:12 -0400

    renamed functions for clarity

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -5,7 +5,7 @@ admin.initializeApp();
 const db = admin.firestore();
 
 // On account creation create a db collection for them with default data
-export const newUserSignUp = functions.auth.user().onCreate((user) => {
+export const onUserSignup = functions.auth.user().onCreate((user) => {
     const defaultDoc = {
         email: user.email,
         name: user.displayName,
@@ -14,7 +14,7 @@ export const newUserSignUp = functions.auth.user().onCreate((user) => {
 });
 
 // On account deletion, delete user data in db
-export const userDeleted = functions.auth.user().onDelete((user) => {
+export const onUserDeleted = functions.auth.user().onDelete((user) => {
     return db.collection('users').doc(user.uid).delete();
 });
 
