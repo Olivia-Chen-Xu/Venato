8314214a04781f7a4cd63637b5d688b80edd5b28
Reid Moffat <18rem8@queensu.ca>
2022-10-12 00:15:24 -0400

    functions add name

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -6,9 +6,11 @@ const db = admin.firestore();
 
 // On account creation create a db collection for them with default data
 export const newUserSignUp = functions.auth.user().onCreate((user) => {
-    return db.collection('users').doc(user.uid).set({
+    const defaultDoc = {
         email: user.email,
-    });
+        name: user.displayName,
+    };
+    return db.collection('users').doc(user.uid).set(defaultDoc);
 });
 
 // On account deletion, delete user data in db
