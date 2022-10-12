cc586f4fd500c084c9020ba575d6115783dd76e9
Reid Moffat <18rem8@queensu.ca>
2022-10-12 00:03:45 -0400

    functions cleanup

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,31 +1,17 @@
 import * as functions from 'firebase-functions';
 import admin = require('firebase-admin');
 
-/**
- * Configure firebase functions
- * Firebase admin is required for functions to access firestore (for security)
- */
-
 admin.initializeApp();
-// const adminAuth = admin.auth();
-// const db = admin.firestore();
-
-/**
- * Helper functions & interfaces
- */
-
-/**
- * Firebase functions to be deployed
- */
+const db = admin.firestore();
 
 // On account creation create a db collection for them with default data
 export const newUserSignUp = functions.auth.user().onCreate((user) => {
-    return admin.firestore().collection('users').doc(user.uid).set({
+    return db.collection('users').doc(user.uid).set({
         email: user.email,
     });
 });
 
 // On account deletion, delete user data in db
 export const userDeleted = functions.auth.user().onDelete((user) => {
-    return admin.firestore().collection('users').doc(user.uid).delete();
+    return db.collection('users').doc(user.uid).delete();
 });
