b5aaabad7b0a765ded59d5d70927b2ab26c65e68
Reid Moffat <18rem8@queensu.ca>
2022-10-20 18:48:42 -0400

    FE states

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,9 +2,67 @@ import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 import { signup, signin, signout, deleteAccount, passwordResetEmail } from '../components/auth';
+import { useState } from "react";
 
 const Hello = () => {
     const nameStyle = { textAlign: 'center' as const };
+
+    const handleSignup = () => {
+        if (currState == 0) {
+            setCurrState(1);
+        } else {
+            if (signup(email, password) == 1) {
+                setCurrState(4);
+            }
+        }
+    }
+
+    const handleSignIn = () => {
+        if (currState == 0) {
+            setCurrState(2);
+        } else {
+            if (signin(email, password) == 1) {
+                setCurrState(4);
+            }
+        }
+    }
+
+    const handlePassReset = () => {
+        if (currState == 0) {
+            setCurrState(3);
+        } else {
+            if (passwordResetEmail() == 1) {
+                setCurrState(0);
+            }
+        }
+    }
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const buttons = {
+        'signup': <button type="submit" onClick={handleSignup}>Sign up</button>,
+        'signin': <button type="submit" onClick={handleSignIn}>Sign in</button>,
+        'signout': <button type="submit" onClick={signout}>Sign out</button>,
+        'deleteAccount': <button type="submit" onClick={deleteAccount}>Delete account</button>,
+        'passwordResetEmail': <button type="submit" onClick={handlePassReset}>Reset password</button>
+    };
+    const states = [
+        <>{buttons['signup']}{buttons['signin']}{buttons['passwordResetEmail']}</>,
+        <><input type="email" value={email} required placeholder="Email"
+                 onChange={(e) => {setEmail(e.target.value)}} />
+            <input type="password" value={password} required placeholder="Password"
+                   onChange={(e) => {setPassword(e.target.value)}} />
+            {buttons['signup']}</>,
+        <><input type="email" value={email} required placeholder="Email"
+                 onChange={(e) => {setEmail(e.target.value)}} />
+            <input type="password" value={password} required placeholder="Password"
+                   onChange={(e) => {setPassword(e.target.value)}} />
+            {buttons['signin']}</>,
+        <>{buttons['passwordResetEmail']}</>,
+        <>{buttons['signout']}{buttons['deleteAccount']}</>
+    ];
+    const [currState, setCurrState] = useState(0);
 
     return (
         <div>
@@ -13,21 +71,7 @@ const Hello = () => {
             </div>
             <h1 style={nameStyle}>electron-react-boilerplate</h1>
             <div className="Hello">
-                <button type="submit" onClick={signup}>
-                    Sign up
-                </button>
-                <button type="submit" onClick={signin}>
-                    Sign in
-                </button>
-                <button type="submit" onClick={signout}>
-                    Sign out
-                </button>
-                <button type="submit" onClick={deleteAccount}>
-                    Delete account
-                </button>
-                <button type="submit" onClick={passwordResetEmail}>
-                    Reset password
-                </button>
+                {states[currState]}
             </div>
         </div>
     );
