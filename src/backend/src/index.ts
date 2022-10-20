2fdcf8cdfe15f98cfc39b877ea6f4ae4dc993d18
Reid Moffat <18rem8@queensu.ca>
2022-10-20 17:22:08 -0400

    stateless functions

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -2,7 +2,6 @@ import * as functions from 'firebase-functions';
 import admin = require('firebase-admin');
 
 admin.initializeApp();
-const db = admin.firestore();
 
 // On account creation create a db collection for them with default data
 export const onUserSignup = functions.auth.user().onCreate((user) => {
@@ -10,12 +9,12 @@ export const onUserSignup = functions.auth.user().onCreate((user) => {
         email: user.email,
         name: user.displayName,
     };
-    return db.collection('users').doc(user.uid).set(defaultDoc);
+    return admin.firestore().collection('users').doc(user.uid).set(defaultDoc);
 });
 
 // On account deletion, delete user data in db
 export const onUserDeleted = functions.auth.user().onDelete((user) => {
-    return db.collection('users').doc(user.uid).delete();
+    return admin.firestore().collection('users').doc(user.uid).delete();
 });
 
 // On user input, add event data in db
