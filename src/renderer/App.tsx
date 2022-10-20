76e48551f2603f703933d184fd63f438feff2d17
Reid Moffat <18rem8@queensu.ca>
2022-10-20 17:24:47 -0400

    re-add buttons

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,30 @@
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import { signup, signin, signout, deleteAccount } from '../components/auth';
 
 const Hello = () => {
+    const nameStyle = { textAlign: 'center' as const };
+
     return (
         <div>
             <div className="Hello">
                 <img width="200" alt="icon" src={icon} />
             </div>
-            <h1>electron-react-boilerplate</h1>
+            <h1 style={nameStyle}>electron-react-boilerplate</h1>
             <div className="Hello">
-                <a
-                    href="https://electron-react-boilerplate.js.org/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <button type="button">
-                        <span role="img" aria-label="books">
-                            📚
-                        </span>
-                        Read our docs
-                    </button>
-                </a>
-                <a
-                    href="https://github.com/sponsors/electron-react-boilerplate"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <button type="button">
-                        <span role="img" aria-label="folded hands">
-                            🙏
-                        </span>
-                        Donate
-                    </button>
-                </a>
+                <button type="submit" onClick={signup}>
+                    Sign up
+                </button>
+                <button type="submit" onClick={signin}>
+                    Sign in
+                </button>
+                <button type="submit" onClick={signout}>
+                    Sign out
+                </button>
+                <button type="submit" onClick={deleteAccount}>
+                    Delete account (must be signed in)
+                </button>
             </div>
         </div>
     );
