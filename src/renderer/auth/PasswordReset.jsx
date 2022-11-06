5051df616d37e4c34d4756bc03bfc9e8c410b73a
Reid Moffat <18rem8@queensu.ca>
2022-11-06 17:51:21 -0500

    prettier formatting

--- src/renderer/auth/PasswordReset.jsx
+++ src/renderer/auth/PasswordReset.jsx
@@ -1,45 +1,41 @@
 const PasswordReset = () => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     return (
-    <div>
-        <text className="TopText">Password reset</text>
-        <br />
-        <br />
-
         <div>
-            <label htmlFor="email">Email</label>
-            <input
-                className="InputForms"
-                type="email"
-                name="email"
-                value={email}
-                placeholder="john.smith@gmail.com"
-                required
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-            />
-        </div>
+            <text className="TopText">Password reset</text>
+            <br />
+            <br />
 
-        <br />
-        {buttons.passwordResetEmail}
-        <p>
-            <text
-                className="SwapAuthTextLink"
-                onClick={() => setCurrState(AuthState.SignIn)}
-            >
-                Sign in
-            </text>
-        </p>
-        <br />
-        {errMsg}
-        <br />
-        {renderEmailSent()}
-    </div>
-)
-}
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    className="InputForms"
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="john.smith@gmail.com"
+                    required
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                />
+            </div>
+
+            <br />
+            {buttons.passwordResetEmail}
+            <p>
+                <text className="SwapAuthTextLink" onClick={() => setCurrState(AuthState.SignIn)}>
+                    Sign in
+                </text>
+            </p>
+            <br />
+            {errMsg}
+            <br />
+            {renderEmailSent()}
+        </div>
+    );
+};
 
 export default PasswordReset;
--- src/renderer/auth/SignIn.tsx
+++ src/renderer/auth/SignIn.tsx
@@ -1,4 +1,6 @@
 const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <div>
