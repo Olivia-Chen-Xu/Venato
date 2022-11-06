bf9e658a5d1d41393cf6b0209711fd10564dc449
D-Joseph <danielrjoseph8@gmail.com>
2022-11-06 18:10:04 -0500

    Cleaned routing code, finished reusable header and added to cal and home pages

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -9,30 +9,37 @@ import ContextWrapper from './calendar/context/ContextWrapper';
 
 export default function AppRoutes() {
     const nav = useNavigate();
-
-    const goHome = () => {
-        nav('/');
-    };
-
-    const goKanban = () => {
-        nav('/kanban');
-    };
-
-    const goJob = () => {
-        nav('/job');
-    };
-
-    const goCal = () => {
-        nav('/calendar');
-    };
-
     return (
         <ContextWrapper>
             <div className="space-x-5">
-                <button onClick={goHome}>Home</button>
-                <button onClick={goKanban}>Kanban</button>
-                <button onClick={goJob}>Job</button>
-                <button onClick={goCal}>Cal</button>
+                <button
+                    onClick={() => {
+                        nav('/home');
+                    }}
+                >
+                    Home
+                </button>
+                <button
+                    onClick={() => {
+                        nav('/kanban');
+                    }}
+                >
+                    Kanban
+                </button>
+                <button
+                    onClick={() => {
+                        nav('/job');
+                    }}
+                >
+                    Job
+                </button>
+                <button
+                    onClick={() => {
+                        nav('/calendar');
+                    }}
+                >
+                    Cal
+                </button>
             </div>
             <Routes>
                 <Route path="/home" element={<Homepage />} />
