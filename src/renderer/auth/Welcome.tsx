6cbd4035895bada54ff79e34646f9cfd0f1bb630
Reid Moffat <18rem8@queensu.ca>
2022-11-10 19:59:57 -0500

    auth bypass

--- src/renderer/auth/Welcome.tsx
+++ src/renderer/auth/Welcome.tsx
@@ -1,8 +1,27 @@
 import { useNavigate } from 'react-router-dom';
 import './auth.css';
+import { useState } from 'react';
+import { signin } from './auth-functions';
 
 const Welcome = () => {
     const navigate = useNavigate();
+    const [errMsg, setErrMsg] = useState('');
+
+    const bypassSignIn = () => {
+        const signInResult = signin('18rem8@queensu.ca', 'Username12345');
+        if (typeof signInResult === 'string') {
+            setErrMsg(`Error signing in to admin account: ${signInResult}`);
+            return;
+        }
+
+        signInResult
+            .then(() => {
+                navigate('/home');
+            })
+            .catch((err) => {
+                setErrMsg(`Error signing in to admin account: ${err}`);
+            });
+    };
 
     return (
         <div className="AuthMainDiv">
@@ -18,10 +37,11 @@ const Welcome = () => {
                 Sign Up
             </button>
 
-            <button type="submit" className="auth-button" onClick={() => navigate('/home')}>
+            <button type="submit" className="auth-button" onClick={bypassSignIn}>
                 Bypass sign-in
             </button>
             <br />
+            {errMsg}
         </div>
     );
 };
