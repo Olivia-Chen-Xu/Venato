0043fefb22d3e44212ee8b1c2eec501a7b78cefd
Reid Moffat <18rem8@queensu.ca>
2022-11-12 17:37:13 -0500

    formatting

--- src/renderer/homepage/Homepage.tsx
+++ src/renderer/homepage/Homepage.tsx
@@ -2,49 +2,49 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Homepage() {
-  const nav = useNavigate();
+    const nav = useNavigate();
 
-  const goKanban = () => {
-    nav('/kanban');
-  };
+    const goKanban = () => {
+        nav('/kanban');
+    };
 
-  return (
-    <div className=''>
-      <header className='w-full mt-5'>
-        <input className="h-65 w-357 text-2xl ml-5" type="text" placeholder="Search" />
-        <button className="float-right">
-          <span className="material-icons-outlined cursor-pointer text-5xl mx-3">
-            settings
-          </span>
-        </button>
-        <div className="float-right cursor-pointer ml-5 bg-gray-200">
-          <button className='py-2 px-3 text-2xl'>
-              Premium
-          </button>
+    return (
+        <div className="">
+            <header className="w-full mt-5">
+                <input className="h-65 w-357 text-2xl ml-5" type="text" placeholder="Search" />
+                <button className="float-right">
+                    <span className="material-icons-outlined cursor-pointer text-5xl mx-3">
+                        settings
+                    </span>
+                </button>
+                <div className="float-right cursor-pointer ml-5 bg-gray-200">
+                    <button className="py-2 px-3 text-2xl">Premium</button>
+                </div>
+                <div className="float-right cursor-pointer bg-gray-200">
+                    <button className="py-2 px-3 text-2xl">Find Jobs</button>
+                </div>
+            </header>
+
+            <h1 className="text-center text-3xl mt-20">Welcome Back _________</h1>
+
+            <div className="mt-20 grid grid-rows-3 gap-y-8 text-3xl">
+                <div className="grid place-content-center">
+                    <button className="px-80 h-32 bg-gray-200" onClick={goKanban}>
+                        View Board 1
+                    </button>
+                </div>
+                <div className="grid place-content-center">
+                    <button className="px-80 h-32 bg-gray-200" onClick={goKanban}>
+                        {' '}
+                        View Board 2{' '}
+                    </button>
+                </div>
+                <div className="grid place-content-center">
+                    <button className="px-80 h-32 bg-gray-200" onClick={goKanban}>
+                        <span className="material-icons-outlined">add_circle</span> Create Board
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="float-right cursor-pointer bg-gray-200">
-          <button className='py-2 px-3 text-2xl'>
-              Find Jobs
-          </button>
-        </div>
-      </header>
-      
-
-
-      <h1 className="text-center text-3xl mt-20">Welcome Back _________</h1>
-
-
-      <div className="mt-20 grid grid-rows-3 gap-y-8 text-3xl">
-        <div className='grid place-content-center'>
-          <button className="px-80 h-32 bg-gray-200" onClick={goKanban}>View Board 1</button>
-        </div>
-        <div className='grid place-content-center'>
-          <button className="px-80 h-32 bg-gray-200" onClick={goKanban}> View Board 2 </button>
-        </div>
-        <div className='grid place-content-center'>
-          <button className="px-80 h-32 bg-gray-200" onClick={goKanban} ><span className='material-icons-outlined'>add_circle</span> Create Board</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
