9a365812b3068f89c0816ff12697340ae7739111
D-Joseph <danielrjoseph8@gmail.com>
2022-11-12 14:35:28 -0500

    Adding routing to sidebar

--- src/renderer/reusable/ReusableSideBar.js
+++ src/renderer/reusable/ReusableSideBar.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ReusableSideBar() {
+    const nav = useNavigate();
     return (
-        <div className="ml-3 mt-5 border-4 border-black-800 material-icons-outlined grid grid-rows-5 gap-y-3 text-5xl h-1/2">
-            <div className='border-2 border-black-400'>circle</div>
-            <div className='border-2 border-black-300'>circle</div>
-            <div className='border-2 border-black-300'>circle</div>
-            <div className='border-2 border-black-300'>circle</div>
-            <div className='border-2 border-black-300'>circle</div>
+        <div className="ml-3 mt-5 bg bg-gray-200 material-icons-outlined grid grid-rows-4 gap-y-3 text-5xl h-1/2">
+            <button onClick={() => nav('/home')}>home</button>
+            <button
+                onClick={() => {
+                    nav('/job');
+                }}
+            >
+                folder
+            </button>
+            <button
+                onClick={() => {
+                    nav('/calendar');
+                }}
+            >
+                calendar_month
+            </button>
+
+            <button
+                onClick={() => {
+                    nav('/job');
+                }}
+            >
+                layers
+            </button>
         </div>
     );
 }
