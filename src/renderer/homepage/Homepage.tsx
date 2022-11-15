7aed28cca299df00d44255cc9f25a7174cdc422c
D-Joseph <danielrjoseph8@gmail.com>
2022-11-14 21:20:35 -0500

    Finished homepage css, added background image

--- src/renderer/homepage/Homepage.tsx
+++ src/renderer/homepage/Homepage.tsx
@@ -7,45 +7,70 @@ export default function Homepage() {
     const nav = useNavigate();
     return (
         <React.Fragment>
-            <h1 className="text-3xl mt-10">Welcome Back!</h1>
+            <h1 className="text-3xl mt-3">Welcome Back!</h1>
 
-            <h1 className="text-2xl mt-10 grid place-content-center ">Upcoming Tasks</h1>
-            <div className="grid grid-cols-3 gap-20 mx-20 h-40 mt-5 ">
-                <div className="grid place-content-center bg-gray-200 rounded-2xl">
-                    <span className="text-3xl">Task</span>
+            <h1 className="text-xl mt-2 grid place-content-center uppercase">Upcoming Tasks</h1>
+            <div className="grid grid-cols-3 gap-20 mx-20 h-40 mt-5 text-white">
+                <div className="place-content-between bg-gradient-to-tl from-[#8080AE] to-[#C7C7E2] rounded-2xl">
+                    <div className='ml-5 mt-5'>
+                        <h1><span className="text-3xl">Interview</span></h1>
+                    </div>
+                  
+                    <div className='ml-5 mt-8'>
+                        <h1 className='text-md align-middle'><span className="material-icons-outlined text-xl">schedule</span> Jan 1st - 4:00 PM</h1>
+                    </div>
+                    <div className='ml-5 mt-1'>
+                        <h1 className='text-md align-middle'><span className="material-icons-outlined text-xl">location_on</span> Zoom</h1>
+                    </div>
                 </div>
-                <div className="grid place-content-center bg-gray-200 rounded-2xl">
-                    <span className="text-3xl">Task</span>
-                </div>
-                <div className="grid place-content-center bg-gray-200 rounded-2xl">
-                    <span className="text-3xl">Task</span>
+                <div className="place-content-between bg-gradient-to-tl from-[#8080AE] to-[#C7C7E2] rounded-2xl">
+                    <div className='ml-5 mt-5'>
+                        <h1><span className="text-3xl">Interview</span></h1>
+                    </div>
+                  
+                    <div className='ml-5 mt-8'>
+                        <h1 className='text-md align-middle'><span className="material-icons-outlined text-xl">schedule</span> Jan 1st - 4:00 PM</h1>
+                    </div>
+                    <div className='ml-5 mt-1'>
+                        <h1 className='text-md align-middle'><span className="material-icons-outlined text-xl">location_on</span> Zoom</h1>
+                    </div>
+                </div><div className="place-content-between bg-gradient-to-tl from-[#8080AE] to-[#C7C7E2] rounded-2xl">
+                    <div className='ml-5 mt-5'>
+                        <h1><span className="text-3xl">Interview</span></h1>
+                    </div>
+                  
+                    <div className='ml-5 mt-8'>
+                        <h1 className='text-md align-middle'><span className="material-icons-outlined text-xl">schedule</span> Jan 1st - 4:00 PM</h1>
+                    </div>
+                    <div className='ml-5 mt-1'>
+                        <h1 className='text-md align-middle'><span className="material-icons-outlined text-xl">location_on</span> Zoom</h1>
+                    </div>
                 </div>
             </div>
 
-            <h1 className="text-2xl mt-10 grid place-content-center">Recent Boards</h1>
-            <div className="mt-5 grid grid-rows-3 gap-y-8 text-3xl">
-                <div className="grid place-content-center">
+            <h1 className="text-xl mt-10 grid place-content-center mx-20 uppercase">Recent Boards</h1>
+            <div className="mt-2 grid grid-rows-2 gap-y-4 text-2xl text-white mx-20 ">
+                <div className="bg-[url('../../assets/home/board.png')] bg-[#793476] bg-right bg-no-repeat bg-contain rounded-2xl">
                     <button
-                        className="px-80 h-32 bg-gray-200"
+                        className="relative w-full h-full py-16"
                         onClick={() => {
                             nav('/kanban');
                         }}
                     >
-                        View Board 1
+                        <span className='absolute bottom-5 left-5 '>Summer Internships 2023</span>
                     </button>
                 </div>
-                <div className="grid place-content-center">
+                <div className="bg-[url('../../assets/home/board.png')] bg-[#793476] bg-right bg-no-repeat bg-contain rounded-2xl">
                     <button
-                        className="px-80 h-32 bg-gray-200"
+                        className="relative w-full h-full"
                         onClick={() => {
                             nav('/kanban');
                         }}
                     >
-                        {' '}
-                        View Board 2{' '}
+                        <span className='absolute bottom-5 left-5'>Summer Internships 2022</span>
                     </button>
                 </div>
-                <div className="grid place-content-center">
+                {/* <div className="grid place-content-center">
                     <button
                         className="px-80 h-32 bg-gray-200"
                         onClick={() => {
@@ -54,7 +79,7 @@ export default function Homepage() {
                     >
                         <span className="material-icons-outlined">add_circle</span> Create Board
                     </button>
-                </div>
+                </div> */}
             </div>
         </React.Fragment>
     );
