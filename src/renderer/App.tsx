4e2e25a4f8302a0b68b199945dc05f6c052c740e
Olivia-Chen-Xu <oliviachenxu@gmail.com>
2022-11-19 19:04:57 -0500

    changed global font + color

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,20 +6,32 @@ import PasswordReset from './auth/PasswordReset';
 import './App.css';
 import Overlay from './reusable/Overlay';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#633175',
+        },
+    },
+});
+
 export default function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Welcome />} />
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/password-reset" element={<PasswordReset />} />
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Welcome />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/password-reset" element={<PasswordReset />} />
 
-                <Route path="/home" element={<Overlay page={'home'} />} />
-                <Route path="/job" element={<Overlay page={'jobs'} />} />
-                <Route path="/kanban" element={<Overlay page={'kanban'} />} />
-                <Route path="/calendar" element={<Overlay page={'cal'} />} />
-            </Routes>
-        </Router>
+                    <Route path="/home" element={<Overlay page={'home'} />} />
+                    <Route path="/job" element={<Overlay page={'jobs'} />} />
+                    <Route path="/kanban" element={<Overlay page={'kanban'} />} />
+                    <Route path="/calendar" element={<Overlay page={'cal'} />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
     );
 }
--- src/renderer/reusable/ReusableSideBar.js
+++ src/renderer/reusable/ReusableSideBar.js
@@ -5,16 +5,20 @@ export default function ReusableSideBar() {
     const nav = useNavigate();
     return (
         <div className="mt-10 mr-10 p-5 rounded-tr-2xl rounded-br-2xl bg-gradient-to-t from-[#ADADD8] to-[#C8ADD8] material-icons-outlined grid grid-rows-4 gap-y-6 text-5xl text-white h-1/2">
-            <button onClick={() => nav('/home')}>home</button>
-             <button
+            <button className="material-icons-outlined text-5xl" onClick={() => nav('/home')}>
+                home
+            </button>
+            <button
+                className="material-icons-outlined text-5xl"
                 onClick={() => {
                     nav('/kanban');
                 }}
             >
                 dashboard
             </button>
-           
+
             <button
+                className="material-icons-outlined text-5xl"
                 onClick={() => {
                     nav('/calendar');
                 }}
@@ -23,6 +27,7 @@ export default function ReusableSideBar() {
             </button>
 
             <button
+                className="material-icons-outlined text-5xl"
                 onClick={() => {
                     nav('/job');
                 }}
