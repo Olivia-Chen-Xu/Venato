388efa4606ebd696b7200df4eaae69dc420e3d38
Reid Moffat <18rem8@queensu.ca>
2022-11-24 00:25:22 -0500

    recent events get

--- src/renderer/calendar/Calendar.tsx
+++ src/renderer/calendar/Calendar.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useAsync } from 'react-async-hook';
 import dayjs from 'dayjs';
 import { getFunctions, httpsCallable } from 'firebase/functions';
-import { CircularProgress } from "@mui/material";
+import { CircularProgress } from '@mui/material';
 import Month from './components/Month';
 import CalendarState from './context/CalendarState';
 import JobDialog from '../job/JobDialog';
@@ -48,6 +48,22 @@ const Calendar = () => {
 
     // Events loaded
     CalendarState.addJobs(jobs.result.data);
+
+    // Get the 3 most immediate tasks
+    const taskDates = Object.entries(CalendarState.events)
+        .map((elem) => elem[0])
+        .sort()
+        .slice(0, 3);
+    const recent = [
+        ...CalendarState.events[taskDates[0]].map((e) => ({ ...e, date: taskDates[0] })),
+        ...CalendarState.events[taskDates[1]].map((e) => ({ ...e, date: taskDates[1] })),
+        ...CalendarState.events[taskDates[2]].map((e) => ({ ...e, date: taskDates[2] })),
+    ].slice(0, 3);
+    const formatDate = (date) => {
+        const split = date.split('-');
+        return `${split[1] === '11' ? 'November' : 'December'} ${(split[2] * 1).toString()}`;
+    };
+
     return (
         <div className="h-screen flex flex-col">
             {modalOpen && (
@@ -63,14 +79,47 @@ const Calendar = () => {
             )}
             <h1 className="grid place-content-center text-3xl mt-5">Upcoming Tasks</h1>
             <div className="grid grid-cols-3 gap-20 mx-20 h-40 my-5">
-                <div className="grid place-content-center bg-gray-200">
-                    <span className="text-3xl">Task</span>
+                <div
+                    className="grid place-content-center bg-gray-200"
+                    onClick={() => {
+                        setCurrentJob(CalendarState.jobs[recent[0].id]);
+                        setModalOpen(true);
+                        setIsEdit(true);
+                    }}
+                >
+                    <text className="text-3xl">{recent[0].title}</text>
+                    {CalendarState.jobs[recent[0].id].position} |{' '}
+                    {CalendarState.jobs[recent[0].id].company}
+                    <br />
+                    {formatDate(recent[0].date)}
                 </div>
-                <div className="grid place-content-center bg-gray-200">
-                    <span className="text-3xl">Task</span>
+                <div
+                    className="grid place-content-center bg-gray-200"
+                    onClick={() => {
+                        setCurrentJob(CalendarState.jobs[recent[1].id]);
+                        setModalOpen(true);
+                        setIsEdit(true);
+                    }}
+                >
+                    <text className="text-3xl">{recent[1].title}</text>
+                    {CalendarState.jobs[recent[1].id].position} |{' '}
+                    {CalendarState.jobs[recent[1].id].company}
+                    <br />
+                    {formatDate(recent[1].date)}
                 </div>
-                <div className="grid place-content-center bg-gray-200">
-                    <span className="text-3xl">Task</span>
+                <div
+                    className="grid place-content-center bg-gray-200"
+                    onClick={() => {
+                        setCurrentJob(CalendarState.jobs[recent[2].id]);
+                        setModalOpen(true);
+                        setIsEdit(true);
+                    }}
+                >
+                    <text className="text-3xl">{recent[2].title}</text>
+                    {CalendarState.jobs[recent[2].id].position} |{' '}
+                    {CalendarState.jobs[recent[2].id].company}
+                    <br />
+                    {formatDate(recent[2].date)}
                 </div>
             </div>
 
@@ -83,7 +132,12 @@ const Calendar = () => {
                         chevron_left
                     </span>
                 </button>
-                <Month month={currentMonth} setOpen={setModalOpen} setJob={setCurrentJob} setIsEdit={setIsEdit} />
+                <Month
+                    month={currentMonth}
+                    setOpen={setModalOpen}
+                    setJob={setCurrentJob}
+                    setIsEdit={setIsEdit}
+                />
                 <button type="button" onClick={() => setMonthIndex(monthIndex + 1)}>
                     <span className="material-icons-outlined cursor-pointer text-6xl text-gray-600 mx-2">
                         chevron_right
