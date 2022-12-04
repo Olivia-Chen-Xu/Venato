f43d4ae0fd242d8648439b753fe85d3dc8de55d0
Reid Moffat <18rem8@queensu.ca>
2022-12-04 12:36:17 -0500

    on job purge, remove associated data

--- src/backend/src/auth.ts
+++ src/backend/src/auth.ts
@@ -9,14 +9,20 @@ import { getDoc } from './helpers';
 // When a user signs up, create a default document for them in firestore
 const onUserSignup = functions.auth.user().onCreate((user: auth.UserRecord) => {
     const defaultDoc = {
-        boards: {}
+        boards: {},
     };
     return getDoc(`users/${user.uid}`).set(defaultDoc);
 });
 
 // On account deletion, delete user data in db (note: don't delete multiple users at the same time with the admin SDK)
 const onUserDeleted = functions.auth.user().onDelete((user: auth.UserRecord) => {
-    // TODO: Delete all jobs this user has created
+    const promises = [];
+
+    getDoc(`users/${user.uid}`)
+        .get()
+        .then((doc) => {})
+        .catch((err) => `Error getting user document: ${err}`);
+
     return getDoc(`users/${user.uid}`).delete();
 });
 
--- src/backend/src/firestore.ts
+++ src/backend/src/firestore.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-import { getDoc } from './helpers';
+import { getCollection, getDoc } from './helpers';
 
 /**
  * Firestore triggers - automatically triggered when a firestore document is changed
@@ -29,11 +29,55 @@ const onJobCreate = functions.firestore.document('jobs/{jobId}').onCreate((snap,
     return Promise.all(promises);
 });
 
-// On job purge (CRON job removed it after 30 days deleted):
+// On job purge (removed by a CRON after being deleted for 30 days):
 // - Remove the company and location from the db if no other jobs have that company/location
 // - Remove the job from the user's job board
 const onJobPurge = functions.firestore.document('jobs/{jobId}').onDelete((snap, context) => {
-    // TODO
+    const promises: Promise<any>[] = [];
+
+    const [id, userId, company, location] = [
+        snap.id,
+        snap.data().userId,
+        snap.data().info.company,
+        snap.data().info.location,
+    ];
+
+    // Remove the company and location from the db if no other jobs have that company/location
+    getCollection('companies')
+        .where('__name__', '==', company)
+        .get()
+        .then((querySnapshot) => {
+            if (querySnapshot.empty) {
+                promises.push(getDoc(`companies/${company}`).delete());
+            }
+        })
+        .catch((err) => `Error getting company document: ${err}`);
+
+    getCollection('locations')
+        .where('__name__', '==', location)
+        .get()
+        .then((querySnapshot) => {
+            if (querySnapshot.empty) {
+                promises.push(getDoc(`location/${location}`).delete());
+            }
+        })
+        .catch((err) => `Error getting company document: ${err}`);
+
+    // Remove the job id from the user's job board
+    getDoc(`users/${userId}`)
+        .get()
+        .then((userDoc) => {
+            const newBoards: object = userDoc.data()?.boards;
+            for (const [key, value] of Object.entries(newBoards)) {
+                if (value.contains(id)) {
+                    newBoards[key] = value.filter((jobId: string) => jobId !== id);
+                }
+            }
+            promises.push(getDoc(`users/${userId}`).update({ boards: newBoards }));
+        })
+        .catch((err) => `Error getting user document: ${err}`);
+
+    return Promise.all(promises);
 });
 
 export { onJobCreate, onJobPurge };
