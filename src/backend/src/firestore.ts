8929d818840a5b35db5fcd46a8324faaa3be3be2
Reid Moffat <18rem8@queensu.ca>
2023-02-05 18:25:42 -0500

    update db triggers

--- src/backend/src/firestore.ts
+++ src/backend/src/firestore.ts
@@ -2,34 +2,8 @@ import * as functions from 'firebase-functions';
 import {
     firestoreHelper,
     getCollection,
-    getDoc,
     getFirestoreTimestamp
 } from './helpers';
-
-/**
- * Db object structures: when adding data to the db, they must follow these structures
- * Note: Value doesn't matter, just use the default
- */
-
-const jobStructure = {
-    details: {
-        position: '',
-        company: '',
-        location: '',
-        link: '',
-        description: '',
-        salary: '',
-    },
-    metaData: {
-        jobId: '',
-    },
-    notes: '',
-    status: {
-        awaitingResponse: false,
-        priority: '',
-        stage: 0,
-    },
-};
 
 /**
  * Firestore triggers - automatically triggered when a firestore document is changed
@@ -45,106 +19,54 @@ const jobStructure = {
  * -Move deadlines, interview questions and contacts to sub-collections
  */
 const onJobCreate = functions.firestore.document('jobs/{jobId}').onCreate(async (snap, context) => {
-    const data = snap.data();
-    const docId = context.params.jobId;
+    // @ts-ignore
+    const job: IJob = snap.data();
     const promises = [];
 
-    // Add searchable job position field
-    const positionSearchable = data.details.position
-        .replace('/[!@#$%^&*()_-+=,:.]/g', '')
-        .toLowerCase()
-        .split(' ');
     promises.push(
         snap.ref.update({
-            metaData: {
-                userId: data.metaData.userId,
-                positionSearchable,
-                boardId: data.metaData.boardId
-            }
+            userId: context.auth?.uid,
         })
     );
 
-    // Add company and location to db
-    if (data.details.company !== '') {
-        const companyDoc = await getDoc(`companies/${data.details.company}`)
-            .get()
-            .then((doc: any) => doc)
-            .catch((err: any) => functions.logger.log(`Error getting company doc: ${err}`));
-        if (companyDoc && companyDoc.exists) {
-            promises.push(companyDoc.ref.update({ numJobs: firestoreHelper.FieldValue.increment(1) }));
-        } else {
-            promises.push(getDoc(`companies/${data.details.company}`).set({ numJobs: 1 }));
-        }
-    }
-
-    if (data.details.location !== '') {
-        const locationDoc = await getDoc(`locations/${data.details.location}`)
-            .get()
-            .then((doc) => doc)
-            .catch((err) => functions.logger.log(`Error getting location doc: ${err}`));
-        if (locationDoc && locationDoc.exists) {
-            promises.push(locationDoc.ref.update({ numJobs: firestoreHelper.FieldValue.increment(1) }));
-        } else {
-            promises.push(getDoc(`locations/${data.details.location}`).set({ numJobs: 1 }));
-        }
-    }
-
     // Move the deadlines to its own collection
-    const { deadlines } = data;
+    const { deadlines } = job;
     promises.push(snap.ref.update({ deadlines: firestoreHelper.FieldValue.delete() }));
 
     deadlines.forEach(
-        (deadline: { title: string; date: number; location: string; link: string }) => {
+        (deadline: IDeadline) => {
             const newDoc = {
                 ...deadline,
                 date: getFirestoreTimestamp(deadline.date),
-                company: data.details.company,
-                metaData: {
-                    userId: data.metaData.userId,
-                    jobId: docId
-                }
+                company: job.company,
+                userId: context.auth?.uid,
             };
             promises.push(getCollection(`deadlines`).add(newDoc));
         }
     );
 
     // Move the interview questions to their own collection (with search params for easy querying)
-    const { interviewQuestions } = data;
+    const { interviewQuestions } = job;
     promises.push(snap.ref.update({ interviewQuestions: firestoreHelper.FieldValue.delete() }));
 
-    interviewQuestions.forEach((question: { name: string; description: string }) => {
+    interviewQuestions.forEach((question: IInterviewQuestion) => {
         const newQuestion = {
             ...question,
-            metaData: {
-                userId: data.metaData.userId,
-                jobId: docId,
-                positionSearchable,
-                company: data.details.company
-            }
+            userId: context.auth?.uid,
+            company: job.company,
         };
         promises.push(getCollection(`interviewQuestions`).add(newQuestion));
     });
 
     // Move the contacts to their own collection
-    const { contacts } = data;
+    const { contacts } = job;
     promises.push(snap.ref.update({ contacts: firestoreHelper.FieldValue.delete() }));
 
     contacts.forEach(
-        (contact: {
-            name: string;
-            email: string;
-            phone: string;
-            company: string;
-            linkedin: string;
-            notes: string;
-            title: string;
-        }) => {
+        (contact: IContact) => {
             const newContact = {
                 ...contact,
-                metaData: {
-                    userId: data.metaData.userId,
-                    jobId: docId
-                }
+                userId: context.auth?.uid,
             };
             promises.push(getCollection(`contacts`).add(newContact));
         }
@@ -160,82 +82,29 @@ const onJobCreate = functions.firestore.document('jobs/{jobId}').onCreate(async 
  * - Remove the deadlines and interview questions (in their own collection)
  */
 const onJobPurge = functions.firestore.document('jobs/{jobId}').onDelete(async (snap, context) => {
+    // @ts-ignore
+    const job: IJob = snap.data();
+    const jobId = snap.id;
     const promises: Promise<FirebaseFirestore.WriteResult>[] = [];
-    const [id, userId, company, location, boardName] = [
-        snap.id,
-        snap.data().userId,
-        snap.data().info.company,
-        snap.data().info.location,
-        snap.data().boardName
-    ];
 
-    // Remove the job id from the user's job board
-    // TODO: this will fail since jobs don't have a board name. Maybe make a sub-collection or
-    // another top-level collection for the job-board relationship?
-    getCollection('boards')
-        .where('userId', '==', userId)
-        .where('name', '==', boardName)
-        .get()
-        .then((board) => {
-            if (board.empty) {
-                throw new functions.https.HttpsError(
-                    'not-found',
-                    `Error: job board ${boardName} not found for user ${userId}`
-                );
-            }
-
-            if (board.docs.length > 1) {
-                throw new functions.https.HttpsError(
-                    'internal',
-                    `Error: more than one job board named ${boardName} for user ${userId}`
-                );
-            }
-
-            const doc = board.docs[0];
-            if (doc.data().jobIds.length === 1 && doc.data().jobIds[0] === id) {
-                promises.push(doc.ref.delete());
-            } else {
-                promises.push(doc.ref.update({ jobs: firestoreHelper.FieldValue.arrayRemove(id) }));
-            }
-            return null;
-        })
-        .catch((err) => functions.logger.log(`Error getting user boards to delete: ${err}`));
-
-    // Decrement the company and location counters
-    const companyDoc = await getDoc(`companies/${company}`)
-        .get()
-        .then((doc) => doc)
-        .catch((err) => console.log(`Error getting company document: ${err}`));
-    if (companyDoc && companyDoc.data()?.numJobs === 1) {
-        promises.push(companyDoc.ref.delete());
-    } else if (companyDoc && companyDoc.data()?.numJobs > 1) {
-        promises.push(companyDoc.ref.update({ numJobs: firestoreHelper.FieldValue.increment(-1) }));
-    }
-
-    const locationDoc = await getDoc(`locations/${location}`)
-        .get()
-        .then((doc) => doc)
-        .catch((err) => console.log(`Error getting company document: ${err}`));
-    if (locationDoc && locationDoc.data()?.numJobs === 1) {
-        promises.push(locationDoc.ref.delete());
-    } else if (locationDoc && locationDoc.data()?.numJobs > 1) {
-        promises.push(
-            locationDoc.ref.update({ numJobs: firestoreHelper.FieldValue.increment(-1) })
-        );
-    }
-
-    // Remove deadlines and interview questions for this job
-    getCollection('deadlines')
-        .where('jobId', '==', id)
+    // Remove deadlines, interview questions and contacts for this job
+    await getCollection('deadlines')
+        .where('jobId', '==', jobId)
         .get()
         .then((deadlines) => deadlines.forEach((deadline) => promises.push(deadline.ref.delete())))
         .catch((err) => functions.logger.log(`Error getting deadlines: ${err}`));
 
-    getCollection('interviewQuestions')
-        .where('jobId', '==', id)
+    await getCollection('interviewQuestions')
+        .where('jobId', '==', jobId)
         .get()
         .then((questions) => questions.forEach((question) => promises.push(question.ref.delete())))
         .catch((err) => functions.logger.log(`Error getting interview questions: ${err}`));
+
+    await getCollection('contacts')
+        .where('jobId', '==', jobId)
+        .get()
+        .then((contacts) => contacts.forEach((contact) => promises.push(contact.ref.delete())))
+        .catch((err) => functions.logger.log(`Error getting contacts: ${err}`));
 
     return Promise.all(promises);
 });
