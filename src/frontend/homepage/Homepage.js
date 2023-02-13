bb734510782c9faa90f6d8987c43b9060c6146fe
Krishaan <krishaanthyagarajan@gmail.com>
2023-02-12 21:53:59 -0500

    adjust styling of homepage

--- src/frontend/homepage/Homepage.js
+++ src/frontend/homepage/Homepage.js
@@ -7,11 +7,15 @@ import taskLine from '../../images/task-line.png';
 import { useState } from 'react';
 import JobDialog from '../reusable/JobDialog';
 
-const bannerStyles = { 
+const buttonStyles = { 
     position: 'relative',
     top: '75px',
     bottom: '0px',
-    left: '75px'
+    left: '75px',
+    background: '#FFFFFF',
+    border: '1px solid #E0E0E0',
+    borderRadius: '8px',
+    color: 'black'
 }
 const Homepage = () => {
     const nav = useNavigate();
@@ -50,13 +54,16 @@ const Homepage = () => {
         const boardsHtml = [];
         userData.result.data.boards.forEach((board) => {
             boardsHtml.push(
-                <div className="bg-[#FFFFFF] border-2 border-black text-black bg-right bg-no-repeat bg-contain rounded-2xl">
+                <div className="font-poppins bg-[#FFFFFF] border-2 border-black text-black bg-right bg-no-repeat bg-contain rounded-2xl">
                     <button
                         className="relative w-full h-full py-16"
                         onClick={() => nav('/kanban', { state: { boardId: board.id } })}
                     >
                         <span className="absolute bottom-5 left-5 ">{board.name}</span>
                     </button>
+                    <svg style={{ position: 'relative', bottom: '100', left: '25' }} width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 7H17M17 7L11 1M17 7L11 13" stroke="#7F5BEB" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                 </div>
             );
         });
@@ -73,14 +80,13 @@ const Homepage = () => {
             (event) => {
                 eventsHtml.push(
                     <div
-                        className="p-5 place-content-between bg-gradient-to-tl from-[#8080AE] to-[#C7C7E2] rounded-2xl"
+                        className="p-5 place-content-between bg-gradient-to-tl from-[#7F5BEB] to-[#7F5BEB] rounded-2xl"
                         onClick={async (mouseEvent) => {
                             const job = await httpsCallable(getFunctions(), 'getJobData')(event.jobId)
                                 .then((result) => result.data);
                             setCurrentJob(job);
                             setModalOpen(true);
-                        }}
-                    >
+                        }}>
                         <div className="ml-5">
                             <h1>
                                 <span className="text-3xl">{event.title}</span>
@@ -136,23 +142,23 @@ const Homepage = () => {
                     isKanban={false}
                 />
             )}
-            <h1 className="text-neutral-500 text-3xl mt-4 ml-20">Welcome Back!</h1>
+            
 
-            <h1 className="text-neutral-500 text-xl mt-2 grid place-content-center uppercase">
+            <h1 className="font-poppins font-medium color-black tracking-wider text-xl mt-10 grid place-content-left mx-20 uppercase">
                 Upcoming Tasks
             </h1>
+
             <div className="grid grid-cols-3 gap-20 mx-20 h-40 my-5" style={{ color: 'white' }}>
                 {renderEvents()}
             </div>
             <br />
             <br />
             <h1 className="font-poppins font-medium color-black tracking-wider text-xl mt-10 grid place-content-left mx-20 uppercase">
-                Get Started 
+                Job Boards
             </h1>
             <h2 className="font-poppins font-light color-black tracking-wider mt-5 grid place-content-left mx-20 uppercase">
                 See what you can accomplish on Venato
             </h2>
-            <br />
             <div className="absolute bottom-100">
                 <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
                     <DialogContent
@@ -193,20 +199,15 @@ const Homepage = () => {
                     </button>
                 </div> */}
             </div>
-            <div className="mt-2 grid grid-cols-3 gap-x-5 text-2xl text-black mx-20">
-                <h1 className="font-poppins font-medium gap-y-5 color-black tracking-wider text-xl uppercase">
-                    Job Boards
-                </h1>
-            </div>
             <Button
-                sx={bannerStyles}        
+                sx={buttonStyles}        
                 color="neutral"
                         variant="contained"
                         startIcon={<AddCircleOutline />}
                         onClick={() => setDialogOpen(true)}
                     >
                         Create new board
-                </Button>
+            </Button>
         </div>
     );
 };
