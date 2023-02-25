4b44df8ab1d3414552b5bb7001d9712d8b0fcc24
Reid Moffat <18rem8@queensu.ca>
2023-02-24 23:44:55 -0500

    api deploy script

---
+++ tools/deployAPI.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# Deploys all your local firebase functions the specified environment (test/staging or production)
+#
+
+if [ "$#" -ne 1 ] || [ "$1" != "TEST" ] && [ "$1" != "STG" ] && [ "$1" != "PROD" ]; then
+  echo " 🔴 1 parameter required: TEST (or STG) or PROD (environment to deploy to)"
+  exit 1
+fi
+
+if [ "$1" == "TEST" ] || [ "$1" == "STG" ]; then
+    echo "  🟢 Deploying to TEST environment..."
+    firebase deploy --only functions --project venato-ae74d
+else
+    echo "  🟢 Deploying to PROD environment..."
+    firebase deploy --only functions --project venato-production-e2ae0
+fi
