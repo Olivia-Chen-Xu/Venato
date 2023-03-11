49340d362584c3742f27e77e859a882be26e9be1
Reid Moffat <18rem8@queensu.ca>
2023-03-10 21:54:30 -0500

    no generate jobs

--- src/frontend/search/JobSearch.js
+++ src/frontend/search/JobSearch.js
@@ -41,11 +41,8 @@ const JobSearch = () => {
     };
 
     const displayJobs = () => {
-        if (!hasSearched) {
-            return <p align="center">Select or enter a search category to search</p>;
-        }
         if (jobs.length === 0) {
-            return <p align="center">⚠️No jobs found; please try another search.</p>;
+            return <p align="center">⚠️No jobs found; please try another search</p>;
         }
 
         return (
